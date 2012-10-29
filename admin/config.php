27b1185aa9e7ed975b8ca38fa4dc4de61ffe8fe0
Shadiya <shadiya.moideen@nsi.com.au>
2012-10-29 09:15:09 +0400

    Added Open Cart to Git

---
+++ admin/config.php
@@ -0,0 +1,32 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost:8888/open_cart/admin/');
+define('HTTP_CATALOG', 'http://localhost:8888/open_cart/');
+define('HTTP_IMAGE', 'http://localhost:8888/open_cart/image/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost:8888/open_cart/admin/');
+define('HTTPS_CATALOG', 'http://localhost:8888/open_cart/');
+define('HTTPS_IMAGE', 'http://localhost:8888/open_cart/image/');
+
+// DIR
+define('DIR_APPLICATION', '/Applications/MAMP/htdocs/open_cart/admin/');
+define('DIR_SYSTEM', '/Applications/MAMP/htdocs/open_cart/system/');
+define('DIR_DATABASE', '/Applications/MAMP/htdocs/open_cart/system/database/');
+define('DIR_LANGUAGE', '/Applications/MAMP/htdocs/open_cart/admin/language/');
+define('DIR_TEMPLATE', '/Applications/MAMP/htdocs/open_cart/admin/view/template/');
+define('DIR_CONFIG', '/Applications/MAMP/htdocs/open_cart/system/config/');
+define('DIR_IMAGE', '/Applications/MAMP/htdocs/open_cart/image/');
+define('DIR_CACHE', '/Applications/MAMP/htdocs/open_cart/system/cache/');
+define('DIR_DOWNLOAD', '/Applications/MAMP/htdocs/open_cart/download/');
+define('DIR_LOGS', '/Applications/MAMP/htdocs/open_cart/system/logs/');
+define('DIR_CATALOG', '/Applications/MAMP/open_cart/upload/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'open_cart');
+define('DB_PREFIX', '');
+?>
---
+++ config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost:8888/open_cart/');
+define('HTTP_IMAGE', 'http://localhost:8888/open_cart/image/');
+define('HTTP_ADMIN', 'http://localhost:8888/open_cart/admin/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost:8888/open_cart/');
+define('HTTPS_IMAGE', 'http://localhost:8888/open_cart/image/');
+
+// DIR
+define('DIR_APPLICATION', '/Applications/MAMP/htdocs/open_cart/catalog/');
+define('DIR_SYSTEM', '/Applications/MAMP/htdocs/open_cart/system/');
+define('DIR_DATABASE', '/Applications/MAMP/htdocs/open_cart/system/database/');
+define('DIR_LANGUAGE', '/Applications/MAMP/htdocs/open_cart/catalog/language/');
+define('DIR_TEMPLATE', '/Applications/MAMP/htdocs/open_cart/catalog/view/theme/');
+define('DIR_CONFIG', '/Applications/MAMP/htdocs/open_cart/system/config/');
+define('DIR_IMAGE', '/Applications/MAMP/htdocs/open_cart/image/');
+define('DIR_CACHE', '/Applications/MAMP/htdocs/open_cart/system/cache/');
+define('DIR_DOWNLOAD', '/Applications/MAMP/htdocs/open_cart/download/');
+define('DIR_LOGS', '/Applications/MAMP/htdocs/open_cart/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'open_cart');
+define('DB_PREFIX', '');
+?>
